736aae87d80b264eb606ead9b64bf2c99e7d8c72
Eric Chan <er_cha@cs.concordia.ca>
2014-02-10 23:06:44 -0500

    NumberFormatException caught during sort

--- src/Tuple.java
+++ src/Tuple.java
@@ -13,8 +13,17 @@ public class Tuple implements Comparable<Tuple> {
 
     @Override
     public int compareTo(Tuple t) {
-        int thiskey = Integer.parseInt(new String(data, 0, 7).trim());
-        int thatkey = Integer.parseInt(new String(t.data, 0, 7).trim());
+        int thiskey = 0;
+        int thatkey = 0;
+
+        try {
+            thiskey = Integer.parseInt(new String(data, 0, 7).trim());
+        } catch (NumberFormatException e) {
+        }
+        try {
+            thatkey = Integer.parseInt(new String(t.data, 0, 7).trim());
+        } catch (NumberFormatException e) {
+        }
         return thiskey - thatkey;
         //return thiskey.compareTo(thatkey);
     }
